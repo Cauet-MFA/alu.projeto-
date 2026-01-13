e566a0738e6bd4dddac0b858461f598a6d5204df
Cauet-MFA <cauet.aguiar@gmail.com>
2026-01-13 03:33:04 -0300

    Add 1-bit full adder module in Verilog

---
+++ full_adder_1bit.v
@@ -0,0 +1,10 @@
+module full_adder_1bit(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+);
+    assign sum  = a ^ b ^ cin;
+    assign cout = (a & b) | (a & cin) | (b & cin);
+endmodule
